b6c86ed9ba5829cc10e519eb56566a87d15531ee
elgenio <rickentchabet@gmail.com>
2025-12-10 13:57:57 +0100

    Implemented problem1 in javascript

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+import { indexOfSum } from "./functions.js";
+
+// Example usage:
+const arr = [2, 7, 11, 15, 17, 8];
+const target = 15;
+const result = indexOfSum(arr, target);
+console.log(result); // Output: [0, 1]
